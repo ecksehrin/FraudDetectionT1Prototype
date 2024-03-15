03b9c6e55e1190945e3f8a0ea226c84fe07f347c
ecksehrin <160687183+ecksehrin@users.noreply.github.com>
2024-03-15 19:47:48 -0400

    Update test.py
    Basic login application

--- test.py
+++ test.py
@@ -1,4 +1,32 @@
-text = input("Enter some text: ") 
-text2 = input("Test")
-print(text) 
-print(text2)
+from hashlib import sha256
+import csv
+
+//Stpring of information, will be put into .csv with hashing later on
+usernames = []
+passwords = []
+names = []
+
+//Register function
+def register():
+    names.append(input("Enter your name:"))
+    usernames.append(input("Enter your username:"))
+    passwords.append(input("Enter your password:"))
+
+//Logging in function
+def login():
+    username = input("Enter your username:")
+    password = input("Enter your password:")
+    if username in usernames and password in passwords:
+        print("Logged in successfully!")
+    else:
+        print("Invalid Login. Try again")
+
+//Menu
+while True:
+    account_ans = input("1. Sign up        2. Login        3. Quit\ns")
+    if account_ans == "1":
+        register()
+    if account_ans == "2":
+        login()
+    if account_ans == "3":
+        break
