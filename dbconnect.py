70d6b537ffbeb6ced1c0428edd811bca13fbf504
ecksehrin <160687183+ecksehrin@users.noreply.github.com>
2024-04-03 17:36:47 -0400

    Create dbconnect.py

---
+++ dbconnect.py
@@ -0,0 +1,25 @@
+pip install pymysql
+import pymysql
+
+events = pymysql.connect(
+    host='frauddetectiont1.database.windows.net',
+    user='team1',
+    password='T1frauddetection',
+    db='Events',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+
+  try:
+    with events.cursor() as cursor:
+        # Read data from database
+        sql = "SELECT * FROM `Events`"
+        cursor.execute(sql)
+
+        # Fetch all rows
+        rows = cursor.fetchall()
+
+        # Print results
+        for row in rows:
+            print(row)
+finally:
+    events.close()
